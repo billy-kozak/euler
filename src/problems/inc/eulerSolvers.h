f0f5a4a3d6b6ed8f063bc3e1489237f933a3a450
Bill <brkozak@gmail.com>
2015-04-17 20:13:05 -0600

    Added stubs for problem 6

--- src/problems/inc/eulerSolvers.h
+++ src/problems/inc/eulerSolvers.h
@@ -16,5 +16,6 @@ struct eulerSol euler_prob2(void);
 struct eulerSol euler_prob3(void);
 struct eulerSol euler_prob4(void);
 struct eulerSol euler_prob5(void);
+struct eulerSol euler_prob6(void);
 //END STUBPROB GENERATION//
 #endif //_EULER_SOLVERS_H_
--- src/problems/probTab.c
+++ src/problems/probTab.c
@@ -18,5 +18,6 @@
 //OPEN STUBPROB GENERATION//
 struct eulerSol (*problemTab[NUM_EULER_PROBLEMS])(void) = {
 	euler_prob1, euler_prob2, euler_prob3, euler_prob4, euler_prob5
+	euler_prob6
 };
 //END STUBPROB GENERATION//
