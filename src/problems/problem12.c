3487f51c8577beb8f99c6204ca49cb9b6d7b854f
Bill <brkozak@gmail.com>
2015-06-04 20:13:41 -0600

    fixed segmentation faults
    The algorithm now seems to be working for input of "7" giving 36. This
    number is important because it is the first to have multiple distinct
    prime power factors with degree greather than 1.
    
    36=2^2+3^2

--- src/problems/problem12.c
+++ src/problems/problem12.c
@@ -142,6 +142,7 @@ static unsigned factorsFromCounts(
 	){
 
 	unsigned c1 = 1<<(pc1->primes+pc2->primes);
+
 	unsigned c2 = 0;
 
 	unsigned levFact = c1>>1;
@@ -149,18 +150,22 @@ static unsigned factorsFromCounts(
 	//for first vector
 	for(unsigned i = 0; i < pc1->powers->len; i++){
 		unsigned powers = w1vect_getIndUnsigned(pc1->powers,i);
+
 		for(unsigned n = 0; n < powers; n++){
 			c2 += levFact;
 		}
-		levFact += (levFact<<1);
+		levFact += (levFact>>1);
 	}
+
+
 	//for second vector
-	for(unsigned i = 0; i < pc1->powers->len; i++){
+	for(unsigned i = 0; i < pc2->powers->len; i++){
 		unsigned powers = w1vect_getIndUnsigned(pc2->powers,i);
+
 		for(unsigned n = 0; n < powers; n++){
 			c2 += levFact;
 		}
-		levFact += (levFact<<1);
+		levFact += (levFact>>1);
 	}
 
 	return c1+c2;
@@ -178,7 +183,11 @@ static unsigned findTriangleDiv(unsigned limit){
 	unsigned factors = 1;
 
 	struct trialPrimeGen* gen = trialDivideGenInit();
-	struct w1_vect* mem = w1vect_init(AUTO,UNSIGNED);
+	struct w1_vect* mem1 = w1vect_init(AUTO,UNSIGNED);
+	struct w1_vect* mem2 = w1vect_init(AUTO,UNSIGNED);
+
+	evenCount.powers = mem1;
+	evenCount.powers = mem2;
 
 	if(!gen){
 		return 0;
@@ -187,7 +196,7 @@ static unsigned findTriangleDiv(unsigned limit){
 	while(factors < limit){
 
 		triN += 1;
-		oddCount = countPrimeFactors(gen,triN+1,mem);
+		oddCount = countPrimeFactors(gen,triN+1,mem1);
 		factors = factorsFromCounts(&oddCount,&evenCount);
 
 		if(factors >= limit){
@@ -195,14 +204,13 @@ static unsigned findTriangleDiv(unsigned limit){
 		}
 
 		triN +=1;
-		evenCount = countPrimeFactors(gen,(triN+1)>>1,mem);
+		evenCount = countPrimeFactors(gen,(triN+1)>>1,mem2);
 		factors = factorsFromCounts(&oddCount,&evenCount);
 	}
 
 	trialDivideGenFree(gen);
-	w1vect_free(mem);
-
-	printf("%d\n",factors);
+	w1vect_free(mem1);
+	w1vect_free(mem2);
 
 	return ((triN+1)*triN)/2;
 }
