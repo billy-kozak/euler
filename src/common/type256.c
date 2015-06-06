ae95c3ab2737719e0ac37ca113d067ca6d0617c3
Bill <brkozak@gmail.com>
2015-06-06 10:13:04 -0600

    added implementation of 256 bit unsigned multiply

--- src/common/type256.c
+++ src/common/type256.c
@@ -7,6 +7,11 @@
 #include "type256.h"
 #include <stdbool.h>
 /******************************************************************************
+*                                   DEFINES                                   *
+******************************************************************************/
+#define MASK_32LOW  (0x00000000FFFFFFFFULL)
+#define MASK_32HIGH (0xFFFFFFFF00000000ULL)
+/******************************************************************************
 *                            FUNCTION DEFINITIONS                             *
 ******************************************************************************/
 /**
@@ -18,22 +23,30 @@ struct unsigned256 _c_uadd256(struct unsigned256* a, struct unsigned256* b){
 	uint64_t carry = 0;
 
 	for(int i = 0; i < 4; i++){
-		y.words[i] = a->words[i]+b->words[i]+carry;
-		carry = (y.words[i] < a->words[i]);
+		y.words.w64[i] = a->words.w64[i]+b->words.w64[i]+carry;
+		carry = (y.words.w64[i] < a->words.w64[i]);
 	}
 
 	return y;
 }
 /**
-*
+* Pure C implementation of 256 bit unsigned multiply
 **/
 struct unsigned256 _c_umul256(struct unsigned256* a, struct unsigned256* b){
-	struct unsigned256 y = {{0}};
 
-	for(int i = 0; i < 4; i++){
-		//uint64_t carry = 0;
+	struct unsigned256 y = {{{0}}};
 
-		for(int n = 0; n < 4; n++){
+	for(int i = 0; i < 8; i++){
+		uint64_t carry = 0;
+		uint64_t bWord = b->words.w32[i];
+
+		for(int n = 0; n < 8-i; n++){
+			uint64_t aWord = a->words.w32[n];
+			uint64_t tmp  = (aWord*bWord);
+			tmp += carry+(uint64_t)y.words.w32[n+i];
+
+			y.words.w32[n+i] = (tmp&MASK_32LOW);
+			carry = (tmp&MASK_32HIGH)>>32;
 		}
 	}
 
