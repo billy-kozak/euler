7385861530ea59d7f66b7b2b98b4abb75938ddf3
Bill <brkozak@gmail.com>
2015-06-10 20:35:13 -0600

    added (untested) division implementation
    also comes with a few helper functions for this purpose

--- src/common/type256.c
+++ src/common/type256.c
@@ -12,8 +12,45 @@
 #define MASK_32LOW  (0x00000000FFFFFFFFULL)
 #define MASK_32HIGH (0xFFFFFFFF00000000ULL)
 /******************************************************************************
+*                             FUNCTION PROTOTYPES                             *
+******************************************************************************/
+static void type256bitSet(struct unsigned256* a, unsigned n, bool val);
+static bool type256bitGet(struct unsigned256* a, unsigned n);
+/******************************************************************************
 *                            FUNCTION DEFINITIONS                             *
 ******************************************************************************/
+/**
+* Sets bit to some value
+**/
+static void type256bitSet(struct unsigned256* a, unsigned n, bool val){
+	if(val){
+		a->words.w64[n>>5] |= 1<<(n&0x1F);
+	}
+	else{
+		a->words.w64[n>>5] &= ~(1<<(n&0x1F));
+	}
+}
+/**
+*
+**/
+static bool type256bitGet(struct unsigned256* a, unsigned n){
+	return !! ( a->words.w64[n>>5]&(1<<(n&0x1F)) );
+}
+/**
+*
+**/
+int type256cmp(struct unsigned256* a,struct unsigned256* b){
+	for( int i = 3; i >= 0; i-- ){
+		if(a->words.w64[i] > b->words.w64[i]){
+			return 1;
+		}
+		else if(a->words.w64[i] < b->words.w64[i]){
+			return -1;
+		}
+	}
+
+	return 0;
+}
 /**
 * Pure C implementation of 256 bit type unsigned addition
 **/
@@ -98,4 +135,23 @@ struct unsigned256 _c_lshift256(struct unsigned256* a, unsigned n){
 	}
 
 	return y;
+}
+/**
+*
+**/
+struct unsigned256 _c_udiv256(struct unsigned256* n, struct unsigned256* d){
+	struct unsigned256 q = {{{0}}};
+	struct unsigned256 r = {{{0}}};
+
+	for(int i = 127; i >= 0; i--){
+		r = _c_lshift256(&r,1);
+		type256bitSet(&r,i,type256bitGet(d,i));
+
+		if(type256cmp(&r,d) >= 0){
+			r = _c_usub256(&r,d);
+			type256bitSet(&q,i,1);
+		}
+	}
+
+	return q;
 }
