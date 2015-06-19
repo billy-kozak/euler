6edbc00c85a91a68f6cbd5c83b14ebe3a7470d04
Bill <brkozak@gmail.com>
2015-06-18 21:15:29 -0600

    have framework for problem 14 solution.
    Doesn't work yet...

--- src/problems/problem14.c
+++ src/problems/problem14.c
@@ -22,14 +22,155 @@
 ******************************************************************************/
 #include "eulerProblems.h"
 #include "eulerSolvers.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+/******************************************************************************
+*                                    TYPES                                    *
+******************************************************************************/
+struct collatzInfo{
+	// zero only when unintilized
+	unsigned seqHead;
+	uint64_t seqPosition;
+	/* non-zero only for head objects (seqPosition==0) */
+	uint64_t seqSize;
+};
+/******************************************************************************
+*                                   DEFINES                                   *
+******************************************************************************/
+#define PROBLEM14_MAGIC (1000000-1)
+/******************************************************************************
+*                             FUNCTION PROTOTYPES                             *
+******************************************************************************/
+static unsigned longestCollatz(unsigned top);
+static uint64_t collatzNext(uint64_t term);
+static int growMap(struct collatzInfo** mPtr,size_t newSize);
+static void collatzAdd(
+		struct collatzInfo* map,uint64_t el,uint64_t head,
+		uint64_t pos
+	);
+static uint64_t collatzDistance(struct collatzInfo* map,uint64_t el);
 /******************************************************************************
 *                            FUNCTION DEFINITIONS                             *
 ******************************************************************************/
 /**
+* Get the next collatz number after the given term
+**/
+static uint64_t collatzNext(uint64_t term){
+	if(term&0x01){
+		return 3*term;
+	}
+
+	return term>>1;
+}
+/**
+* Get the distance to '1' from the given element in the collatz map
+**/
+static uint64_t collatzDistance(struct collatzInfo* map,uint64_t el){
+	struct collatzInfo* head = &map[map[el].seqHead];
+	uint64_t pos = map[el].seqPosition;
+
+	if(!head->seqSize){
+		//error - we've hit a loop (but more likley a bug...)!
+		return 0;
+	}
+
+	return head->seqSize - pos;
+}
+/**
+* Add value to collatz map
+**/
+static void collatzAdd(
+		struct collatzInfo* map,uint64_t el,uint64_t head,
+		uint64_t pos
+	){
+	map[el].seqHead = head;
+	map[el].seqPosition = pos;
+}
+/**
+* Grows map to fit the new size
+**/
+static int growMap(struct collatzInfo** mPtr,size_t newSize){
+	struct collatzInfo* tmp = realloc(*mPtr,newSize*sizeof(*tmp));
+	if(!tmp){
+		return 1;
+	}
+
+	*mPtr = tmp;
+	return 0;
+}
+/**
+* Finds the longest collatz chain starting on any num <= top
+**/
+static unsigned longestCollatz(unsigned top){
+
+	uint64_t maxSize = 0;
+	uint64_t head = top;
+	size_t mapSize = 4*(top+1);
+	struct collatzInfo* collatzMap;
+
+	if(head == 1){
+		return 1;
+	}
+
+	collatzMap = calloc(mapSize,sizeof(collatzMap));
+
+	if(!collatzMap){
+		goto fail;
+	}
+
+	while(head){
+		uint64_t next = head;
+		uint64_t count = 1;
+
+		collatzMap[head].seqPosition = 0;
+		collatzMap[head].seqHead = head;
+
+		do{
+			next = collatzNext(next);
+
+			if(next >= mapSize){
+				mapSize *= 2;
+				if(growMap(&collatzMap,mapSize)){
+					goto fail;
+				}
+			}
+
+			if(!collatzMap[next].seqHead){
+				collatzAdd(collatzMap,next,head,count);
+				count+=1;
+			}
+			else{
+				uint64_t partial = collatzDistance(
+						collatzMap,next
+					);
+
+				if(!partial){
+					// we actually seem to have a loop!
+					goto fail;
+				}
+				count += partial;
+				break;
+			}
+		}while(next != 1);
+
+		collatzMap[head].seqSize = count;
+		maxSize = (maxSize < count) ? count : maxSize;
+		for(head = head-1; collatzMap[head].seqHead; head--);
+	}
+
+
+	free(collatzMap);
+	return maxSize;
+fail:
+	free(collatzMap);
+	return 0;
+}
+/**
 * Solution for problem 14
 **/
 struct eulerSol euler_prob14(void){
-	struct eulerSol sol = {U64};
-
+	struct eulerSol sol = {UNSIGNED};
+	sol.val.u = longestCollatz(PROBLEM14_MAGIC);
 	return sol;
 }
