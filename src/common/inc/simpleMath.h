9fa3f78c4a3b29dff48cc17448e3f2ba4b320ee3
Bill <brkozak@gmail.com>
2015-06-23 20:50:53 -0600

    have reasonably fast solution
    The naive solution is reasonably fast in this case, we can just use this
    (at least for now).

--- src/common/inc/simpleMath.h
+++ src/common/inc/simpleMath.h
@@ -14,9 +14,23 @@
 #else
 #define umul_overflow(a,b,res) slow_umul_overflow(a,b,res)
 #endif
+
+#ifdef __GNUC__
+#define count_tzll(n) __builtin_ctzl(n)
+#else
+#define count_tzll(n) slow_count_tzll(n)
+#endif
 /******************************************************************************
 *                              INLINE FUNCTIONS                               *
 ******************************************************************************/
+/**
+* Counts trailing zeroes using slow pure C implementation
+**/
+static inline int slow_count_tzll(unsigned long long n){
+	int cnt;
+	for(cnt = 0; !(n&(1<<cnt)); cnt++);
+	return cnt;
+}
 /**
 * Returns a number which is greater than or equal to sqrt of n
 *
--- src/problems/problem14.c
+++ src/problems/problem14.c
@@ -22,6 +22,7 @@
 ******************************************************************************/
 #include "eulerProblems.h"
 #include "eulerSolvers.h"
+#include "simpleMath.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -43,66 +44,29 @@ struct collatzInfo{
 *                             FUNCTION PROTOTYPES                             *
 ******************************************************************************/
 static unsigned longestCollatz(unsigned top);
-static uint64_t collatzNext(uint64_t term);
-static int growMap(struct collatzInfo** mPtr,size_t sNew, size_t sOld);
-static void collatzAdd(
-		struct collatzInfo* map,uint64_t el,uint64_t head,
-		uint64_t pos
-	);
-static uint64_t collatzDistance(
-		struct collatzInfo* map,uint64_t* collatzHeads, uint64_t el
-	);
 /******************************************************************************
 *                            FUNCTION DEFINITIONS                             *
 ******************************************************************************/
 /**
-* Get the next collatz number after the given term
+* COunts length of collatz sequence which starts with the given term
 **/
-static uint64_t collatzNext(uint64_t term){
-	if(term&0x01){
-		return 3*term+1;
+static uint64_t collatzLen(uint64_t term){
+	uint64_t count = 1;
+	uint64_t next = term;
+
+	while(next != 1){
+		int tz = count_tzll(next);
+		if(tz){
+			count += tz;
+			next = next>>tz;
+		}
+		else{
+			count += 1;
+			next = next*3+1;
+		}
 	}
 
-	return term>>1;
-}
-/**
-* Get the distance to '1' from the given element in the collatz map
-**/
-static uint64_t collatzDistance(
-		struct collatzInfo* map,uint64_t* collatzHeads, uint64_t el
-	){
-
-	uint64_t seqSize = collatzHeads[map[el].seqHead];
-	uint64_t pos = map[el].seqPosition;
-
-	if(!seqSize){
-		//error - we've hit a loop (but more likley a bug...)!
-		return 0;
-	}
-
-	return seqSize - pos;
-}
-/**
-* Add value to collatz map
-**/
-static void collatzAdd(
-		struct collatzInfo* map,uint64_t el,uint64_t head,
-		uint64_t pos
-	){
-	map[el].seqHead = head;
-	map[el].seqPosition = pos;
-}
-/**
-* Grows map to fit the new size, makes sure that new memory is zeroed
-**/
-static int growMap(struct collatzInfo** mPtr,size_t sNew, size_t sOld){
-	struct collatzInfo* tmp = realloc(*mPtr,sNew*sizeof(*tmp));
-	if(!tmp){
-		return 1;
-	}
-	*mPtr = tmp;
-	memset(tmp+sOld,0,(sNew-sOld));
-	return 0;
+	return count;
 }
 /**
 * Finds the longest collatz chain starting on any num <= top
@@ -111,69 +75,15 @@ static unsigned longestCollatz(unsigned top){
 
 	uint64_t maxSize = 0;
 	uint64_t head = top;
-	size_t mapSize = 4*(top+1);
-	struct collatzInfo* collatzMap;
-	uint64_t* collatzHeads;
-
-	if(head == 1){
-		return 1;
-	}
-
-	collatzHeads = calloc(sizeof(*collatzHeads),top+1);
-	collatzMap = calloc(mapSize,sizeof(*collatzMap));
-
-	if(!collatzMap || !collatzHeads){
-		goto fail;
-	}
 
 	while(head){
-		uint64_t next = head;
-		uint64_t count = 1;
-
-		collatzMap[head].seqPosition = 0;
-		collatzMap[head].seqHead = head;
-
-		do{
-			next = collatzNext(next);
-
-			if(next >= mapSize){
-				if(growMap(&collatzMap,next+1,mapSize)){
-					goto fail;
-				}
-				mapSize = next+1;
-			}
-
-			if(!collatzMap[next].seqHead){
-				collatzAdd(collatzMap,next,head,count);
-				count+=1;
-			}
-			else{
-				uint64_t partial = collatzDistance(
-						collatzMap,collatzHeads,next
-					);
-
-				if(!partial){
-					// we actually seem to have a loop!
-					goto fail;
-				}
-				count += partial;
-				break;
-			}
-		}while(next != 1);
-
-		collatzHeads[head] = count;
+		uint64_t count = collatzLen(head);
 		maxSize = (maxSize < count) ? count : maxSize;
-		for(head = head-1; collatzHeads[head]; head--);
+
+		head -=1;
 	}
 
-
-	free(collatzMap);
-	free(collatzHeads);
 	return maxSize;
-fail:
-	free(collatzMap);
-	free(collatzHeads);
-	return 0;
 }
 /**
 * Solution for problem 14
